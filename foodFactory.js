f4e6b2a441272b5fea234e85f27204b9f1e4db4a
Student <student@RU-003.local>
2016-02-03 10:43:36 -0700

    writing part II constructors

---
+++ foodFactory.js
@@ -0,0 +1,60 @@
+angular.module('app')
+	.factory('foodFactory', [function() {
+
+		function FoodItem (name, calories, vegan, glutenFree, citrusFree ) {
+			this.name = name;
+			this.calories = calories;
+			this.vegan = vegan;
+			this.glutenFree = glutenFree;
+			this.citrusFree = citrusFree;
+		}
+
+		FoodItem.prototype.stringify = function() {
+			var isVegan = this.vegan ? "is" : "is not";
+			var isGF = this.glutenFree ? "is" : "is not";
+			var isCF = this.citrusFree ? "is" : "is not";
+			return this.name + " has " + this.calories +" calories, " + isVegan + " vegan, " + isGF + " gluten free, and " + isCF + " citrus free."
+		}
+
+		Drink.prototype.stringify = function() {
+			var ingredients = this.ingredients.reduce(function(prev,cur) { return prev.name + cur.name})
+			return this.name + " is " + this.desc + "It contains " + ingredients + " and costs " + this.price + "."
+		}
+
+		function Drink (name, desc, price, ingredients) {
+			this.name = name;
+			this.desc = desc;
+			this.price = price;
+			this.ingredients = ingredients;
+		}
+
+		function Plate (name, desc, price, ingredients) {
+			this.name = name;
+			this.desc = desc;
+			this.price = price;
+			this.ingredients = ingredients;
+		}
+
+		function Order (plates) {
+			this.plates = plates;
+		}
+
+		function Menu (plates) {
+			this.plates = plates;
+		}
+
+		function Restaurant (name, desc, menu) {
+			this.name = name;
+			this.desc = desc;
+			this.menu = menu;
+		}
+
+		function Customer (dietaryPreferance) {
+			this.dietaryPreferance = dietaryPreferance
+		}
+
+	}])
+
+	var burrito = new FoodItem('burrito', 1000, false, false, false)
+	var margarita = new FoodItem('margarita', 666, true, true, false)
+	var butter = new FoodItem('butter', 500, true, true, true)
